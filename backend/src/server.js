68079621a15e129c2bf0e3168c5dbafb3e83fa51
arthurRocha01 <arthurrocha007.net@gmail.com>
2026-03-19 15:31:41 -0300

    chore(backend): comenta inicialização do node para deploy na vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,28 +7,21 @@ import { userRouter } from './routes/userRoutes.js';
 
 const app = express();
 
-// necessário no ESModules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// middlewares
 app.use(express.json());
 app.use(cors());
 
-// 👇 CAMINHO PARA A PASTA FRONTEND (FORA DO BACKEND)
 const frontendPath = path.join(__dirname, '../../frontend');
-
-// servir arquivos estáticos
 app.use(express.static(frontendPath));
 
-// rotas da API
 app.use('/api', userRouter);
 
-// abrir index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(frontendPath, 'index.html'));
+// });
 
-app.listen(3030, () => {
-  console.log('Servidor rodando em http://localhost:3030');
-});
+// app.listen(3030, () => {
+//   console.log('Servidor rodando em http://localhost:3030');
+// });
